1e69257b736b7b2c51627e1dd1a4216dd6931b19
bleachjade <bleachjade@hotmail.com>
2021-01-12 17:05:04 +0700

    add Info model so you can change the info later

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Info(models.Model):
+    profile_pic = models.ImageField("Profile picture", upload_to='profile_pics/', height_field=None, width_field=None, max_length=None)
+    name = models.CharField("Full name", max_length=50, default='Jade')
+    nick_name = models.CharField("Nickname", max_length=20)
+    description = models.CharField("description about yourself", max_length=300)
+    email = models.EmailField("email", max_length=254)
+    resume = models.FileField("resume", upload_to='resumes/', max_length=100)
