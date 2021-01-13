75f334272bba807e623d8f84340f1d6f144ab110
bleachjade <bleachjade@hotmail.com>
2021-01-13 21:30:36 +0700

    add work to view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponseRedirect
-from .models import Info
+from .models import Info, Work
 
 def index(request):
     info = Info.objects.all()
-    return render(request, 'portfolio/index.html', {'info': info})
+    work = Work.objects.all()
+    return render(request, 'portfolio/index.html', {'info': info, 'work': work})
     
