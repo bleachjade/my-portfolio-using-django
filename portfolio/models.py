b932f1ce062e8a7e61bf237558a80abeb0060ee8
bleachjade <bleachjade@hotmail.com>
2021-01-13 21:30:24 +0700

    fix work model

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,10 +9,11 @@ class Info(models.Model):
     email = models.EmailField("email", max_length=254)
     resume = models.FileField("resume", upload_to='resumes/', max_length=100)
 
-class Works(models.Model):
-    work_image = models.ImageField('Work image', upload_to='upload/', height_field=None, width_field=None, max_length=None)
-    work_description = models.CharField("Work description", max_length=200)
-
+class Work(models.Model):
+    project_image = models.ImageField('Project image', upload_to='upload/', height_field=None, width_field=None, max_length=None)
+    project_name = models.CharField('Project name', max_length=50)
+    project_description = models.CharField("Project description", max_length=200)
+    github_project = models.URLField('Github URL', max_length=128, default='https://github.com/bleachjade')
 
     def get_image(self):
-        return self.work_image
+        return self.project_image
