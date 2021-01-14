83c083350237c72704980df616dd2a213bde620e
bleachjade <bleachjade@hotmail.com>
2021-01-14 22:25:25 +0700

    add media root, url, and staticfiles dirs

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,4 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = ('portfolio/static',)
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
