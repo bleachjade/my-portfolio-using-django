ba91d46a3ba335b0b4d7e578a6627f25fdbb3eaf
bleachjade <bleachjade@hotmail.com>
2021-01-15 13:58:00 +0700

    add contact form and model in index page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render
-from django.views import generic
-from django.http import HttpResponseRedirect
-from .models import Info, Work
+from django.shortcuts import render, reverse, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.core.mail import send_mail, BadHeaderError
+from .models import Info, Work,Contact
+from .forms import ContactForm
+
 
 def index(request):
     info = Info.objects.all()
     work = Work.objects.all()
-    return render(request, 'portfolio/index.html', {'info': info, 'work': work})
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            contact = Contact(from_email=from_email, subject=subject, message=message)
+            try:
+                send_mail(subject, message, from_email, ['nattapol.boo@ku.th'])
+                contact.save()
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, 'portfolio/index.html', {'info': info, 'work': work, 'form': form})
+
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
+
     
