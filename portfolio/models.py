914a6d48d8c7a20a2b284190e6bb504791f36666
bleachjade <bleachjade@hotmail.com>
2021-01-16 14:11:54 +0700

    change upload dir to cloud

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class Info(models.Model):
-    profile_pic = models.ImageField("Profile picture", upload_to='upload/', height_field=None, width_field=None, max_length=None)
+    profile_pic = CloudinaryField("profile_pic")
     name = models.CharField("Full name", max_length=50, default='Jade')
     nick_name = models.CharField("Nickname", max_length=20)
     description = models.CharField("description about yourself", max_length=300)
@@ -15,7 +16,7 @@ class Info(models.Model):
         return self.profile_pic
 
 class Work(models.Model):
-    project_image = models.ImageField('Project image', upload_to='upload/', height_field=None, width_field=None, max_length=None)
+    project_image = CloudinaryField("project_img")
     project_name = models.CharField('Project name', max_length=50)
     project_description = models.CharField("Project description", max_length=200)
     github_project = models.URLField('Github URL', max_length=128, default='https://github.com/bleachjade')
