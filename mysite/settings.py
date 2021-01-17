b96ba19a27df6bc7952b352c380917ae2819df59
bleachjade <bleachjade@hotmail.com>
2021-01-17 17:07:15 +0700

    fix password in env

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,7 +135,7 @@ SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'jade26072542@gmail.com'
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='dummy-api-key')
+EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD', default='dummy-api-key')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
